69609dedc55cd87d4b829b281f11c2057fda4bb1
SHIVA Gupta <coolshiva.gandhi@gmail.com>
2017-01-05 04:54:39 +0000

    first commit

---
+++ bmp/resize.c
@@ -0,0 +1,156 @@
+/**
+ * copy.c
+ *
+ * Computer Science 50
+ * Problem Set 4
+ *
+ * Copies a BMP piece by piece, just because.
+ */
+       
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bmp.h"
+
+int main(int argc, char* argv[])
+{
+    // ensure proper usage
+    if (argc != 4)
+    {
+        printf("Usage: ./copy infile outfile\n");
+        return 1;
+    }
+    
+    int fact = atoi(argv[1]);
+    
+     // ensure proper usage
+    if (fact < 1 || fact > 100  )
+    {
+        printf("Usage: ./copy infile outfile\n");
+        return 5;
+    }
+
+
+    
+    char* infile = argv[2];
+    char* outfile = argv[3];
+
+    // open input file 
+    FILE* inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        printf("Could not open %s.\n", infile);
+        return 2;
+    }
+
+    // open output file
+    FILE* outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        fprintf(stderr, "Could not create %s.\n", outfile);
+        return 3;
+    }
+
+    // read infile's BITMAPFILEHEADER
+    BITMAPFILEHEADER bf, bf_new;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+   
+    bf_new = bf;
+
+    // read infile's BITMAPINFOHEADER
+    BITMAPINFOHEADER bi, bi_new;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+    
+    bi_new = bi;
+    
+   
+
+    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || 
+        bi.biBitCount != 24 || bi.biCompression != 0)
+    {
+        fclose(outptr);
+        fclose(inptr);
+        fprintf(stderr, "Unsupported file format.\n");
+        return 4;
+    }
+    
+    bi_new.biWidth = bi.biWidth * fact;
+    bi_new.biHeight = bi.biHeight * fact;
+    int Padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    int newPadding = (4 - (bi_new.biWidth * sizeof(RGBTRIPLE) ) % 4) % 4;
+     
+    bf_new.bfSize = 54 + bi_new.biWidth * abs(bi_new.biHeight) * sizeof(RGBTRIPLE) + newPadding * abs(bi_new.biHeight);
+     
+    // write outfile's BITMAPFILEHEADER
+    fwrite(&bf_new, sizeof(BITMAPFILEHEADER), 1, outptr);
+  
+  
+    
+   // bi.biSizeImage = fact * (bi.biWidth* sizeof(RGBTRIPLE) + newPadding)*fact  * abs(bi.biHeight);
+    bi_new.biSizeImage = ((((bi_new.biWidth * bi_new.biBitCount) + 31) & ~31) / 8) * abs(bi_new.biHeight);
+    
+    
+    // write outfile's BITMAPINFOHEADER
+    fwrite(&bi_new, sizeof(BITMAPINFOHEADER), 1, outptr);
+
+    
+    // iterate over infile's scanlines
+    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
+    {
+        RGBTRIPLE arr[bi_new.biWidth];
+        
+        // iterate over pixels in scanline
+        for (int j = 0, J = 0; j < bi.biWidth; j++)
+        {
+            // temporary storage
+            RGBTRIPLE triple;
+
+            // read RGB triple from infile
+            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+
+            for(int k = 0; k < fact ; k++)
+            {
+                arr[J] = triple;
+                J++;
+                fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+            }
+            
+        }
+         // skip over padding, if any
+        fseek(inptr, Padding, SEEK_CUR);
+
+        // then add it back (to demonstrate how)
+        for (int k = 0; k < newPadding; k++)
+        {
+            fputc(0x00, outptr);
+        }
+        
+        for(int f = 1;f < fact; f++)
+        {
+       
+            for(int i1 = 0 ; i1 < bi_new.biWidth; i1++)
+                fwrite(&arr[i1], sizeof(RGBTRIPLE), 1, outptr);
+         // skip over padding, if any
+        
+            for (int k = 0; k < newPadding; k++)
+            {
+                fputc(0x00, outptr);
+            }
+        
+        }
+        
+    }
+
+    // close infile
+    fclose(inptr);
+
+    // close outfile
+    fclose(outptr);
+
+    // that's all folks
+    return 0;
+}
+ 
+ 
---
+++ jpg/recover.c
@@ -0,0 +1,104 @@
+/**
+ * recover.c
+ *
+ * Computer Science 50
+ * Problem Set 4
+ *
+ * Recovers JPEGs from a forensic image.
+ */
+# include<stdio.h>
+# include<math.h>
+# include<stdlib.h>
+
+int main(int argc , char* argv[])
+{
+    //printf("program started\n");
+    argv[0] = argv[0];
+    int jpeg = 0;
+    int hundred = 0;
+    int tens =0;
+    int unit =0;
+   // char* format = ".jpg";
+    char filename[8] ;
+    
+    if(argc != 1)
+    {
+        printf("Usage: ./copy infile outfile\n");
+        return 1;
+    }
+    char* InFile = "card.raw";
+    //printf("card.raw in file created\n");
+    FILE* inptr = fopen(InFile , "r" );
+    FILE* outptr = NULL;
+    if(inptr == NULL)
+    {
+        printf("Usage: ./copy infile outfile\n");
+        fclose(inptr);
+        return 2;
+    }
+    //printf("inptr to i file created\n");
+   // unsigned char* bytes = (unsigned char*) malloc(512); 
+      unsigned char bytes[512];
+     // printf("bytes created\n");
+    
+    //int number=1;
+    while(fread(&bytes,512,1,inptr) )
+    {
+      /*  if(bytes == NULL)
+        {
+            break;
+        } */
+        //printf("entered while \n");
+        if(bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff && ((bytes[3] & 0xe0) == 0xe0))
+        {   
+            //printf("got the star \n");
+            if(jpeg == 0)
+            {
+               // printf("first star \n");
+                sprintf(filename,"00%d.jpg",(jpeg));
+                //printf("sprintf worked  \n");
+                outptr = fopen(filename,"a");
+                //printf("outptr file opened \n");
+                if(outptr == NULL)
+                {
+                    printf("Usage: ./copy infile outfile\n");
+                    fclose(inptr);
+                    fclose(outptr);
+                   return 3;
+                }
+                jpeg++;
+            }
+            else
+            {
+                unit = jpeg % 10;
+                tens = (jpeg/10) % 10;
+                hundred = (jpeg/100) % 10;
+                //printf("next star \n");
+                fclose(outptr);
+                sprintf(filename,"%d%d%d.jpg",hundred,tens,(unit));
+                outptr = fopen(filename,"a");
+                if(outptr == NULL)
+                {
+                    printf("Usage: ./copy infile outfile\n");
+                    fclose(inptr);
+                    fclose(outptr);
+                   return 3;
+                }
+                jpeg++;
+            }
+        }
+        if(jpeg > 0)
+        {
+        //printf("writeen in outptr \n");
+        fwrite(&bytes,512,1,outptr);
+        }
+        /*else
+        printf(" not written in file \n");*/
+        
+    }
+    //printf("reached end \n");
+    //free(bytes);
+    fclose(inptr);
+    fclose(outptr);
+    return 0;
+}
